69e729f494e013e2aeaa5183a37079d736396450
⚡ Sigui <siguici@proton.me>
2025-02-28 15:09:48 +0100

    🧑‍💻 Add Bootsrap file

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(-1);
+
+ini_set('display_errors', '1');
+
+require __DIR__.'/vendor/autoload.php';
--- public/index.php
+++ public/index.php
@@ -2,4 +2,6 @@
 
 declare(strict_types=1);
 
+require_once __DIR__.'/../bootstrap.php';
+
 echo '<p>Welcome to the S project!</p>';
