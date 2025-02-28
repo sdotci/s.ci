a4328cb967b682502d6f1a4e7babd65e195c1a97
⚡ Sigui <siguici@proton.me>
2025-02-28 15:27:25 +0100

    🚧 Use the S router

--- public/index.php
+++ public/index.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+use S\Foundation\Http\Router;
+
 require_once __DIR__.'/../bootstrap.php';
 
-echo '<p>Welcome to the S project!</p>';
+$router = new Router();
+
+$router->on('GET /', fn() => '<p>Welcome to S!</p>');
+
+$router->run(($_SERVER['REQUEST_METHOD'] ?? 'GET') . ' ' . ($_SERVER['REQUEST_URI'] ?? '/'));
