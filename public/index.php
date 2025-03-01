7f6720fe8cc7235dca0637c90fff3b74b2422a2f
⚡ Sigui <siguici@proton.me>
2025-03-01 22:00:02 +0100

    ✨ Use context attributes

--- public/index.php
+++ public/index.php
@@ -9,6 +9,6 @@ require_once __DIR__.'/../bootstrap.php';
 
 $app = new Application(Context::global());
 
-$app->on('GET /', fn (Context $context) => '<p>Hello '.$context->getInput()->get('REQUEST_URI', 'World').'!</p>');
+$app->on('GET /{name}', fn (Context $context) => '<p>Hello '.$context->getAttribute('name', 'World').'!</p>');
 
 $app->run();
