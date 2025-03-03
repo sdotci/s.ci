6b128745905c6e8d6d296c61ca4468c8a21f82b6
⚡ Sigui <siguici@proton.me>
2025-03-03 01:02:04 +0100

    🚸 Better manage optional parameters

--- public/index.php
+++ public/index.php
@@ -9,6 +9,6 @@ require_once __DIR__.'/../bootstrap.php';
 
 $app = new Application(Context::global());
 
-$app->on('GET /{name}', fn (?string $name) => '<p>Hello '.($name ?? 'World').'!</p>');
+$app->on('GET /{name}?', fn (string $name) => '<p>Hello '.($name ?: 'World').'!</p>');
 
 $app->run();
