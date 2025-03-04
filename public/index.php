2c67ab9268c97a35f3df1e32e31002b7368413a6
⚡ Sigui <siguici@proton.me>
2025-03-04 16:49:06 +0100

    🧑‍💻 Use the default global context

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use S\Foundation\Application;
 use S\Http\Context;
 
 /** @var Application $app */
-$app = (require_once __DIR__.'/../src/app.php')(Context::global($_SERVER));
+$app = (require_once __DIR__.'/../src/app.php')(Context::global());
 
 $app->on('GET /{name}?', fn (string $name) => '<p>Hello '.($name ?: 'World').'!</p>');
 
